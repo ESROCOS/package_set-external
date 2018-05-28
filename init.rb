adc23b48089cb17e53cf63d59b24e27fc08a9549
Raquel Jalvo <rjalvo@gmv.com>
2018-05-28 14:48:16 +0200

    Added RoboticArm libraries

--- init.rb
+++ init.rb
@@ -2,3 +2,7 @@ require 'autoproj/gitorious'
 if !Autoproj.has_source_handler? 'github'
     Autoproj.gitorious_server_configuration('GITHUB', 'github.com', :http_url => 'https://github.com')
 end
+
+if !Autoproj.has_source_handler? 'spass-git'
+    Autoproj.gitorious_server_configuration('GITLAB', 'spass-git-ext.gmv.com', :http_url => 'https://spass-git-ext.gmv.com')
+end
