77d03906910671422142c86572316ae2f5e32039
Raquel Jalvo <rjalvo@gmv.com>
2018-05-29 08:56:08 +0200

    Fixed the spassgit repository url

--- init.rb
+++ init.rb
@@ -3,6 +3,7 @@ if !Autoproj.has_source_handler? 'github'
     Autoproj.gitorious_server_configuration('GITHUB', 'github.com', :http_url => 'https://github.com')
 end
 
-if !Autoproj.has_source_handler? 'spass-git'
-    Autoproj.gitorious_server_configuration('GITLAB', 'spass-git-ext.gmv.com', :http_url => 'https://spass-git-ext.gmv.com')
+if !Autoproj.has_source_handler? 'spassgit'
+    Autoproj.gitorious_server_configuration('SPASSGIT', 'spass-git-ext.gmv.com', :http_url => 'https://spass-git-ext.gmv.com')
 end
+
